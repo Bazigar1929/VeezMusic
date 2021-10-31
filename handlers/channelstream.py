a73c431ef962bfed00df66d4702d6120477d9a20
Bazigar X Bots <89571345+Bazigar1929@users.noreply.github.com>
2021-10-31 16:23:45 +0530

    Update channelstream.py

--- handlers/channelstream.py
+++ handlers/channelstream.py
@@ -430,7 +430,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
+        thumb_name = "https://telegra.ph/file/e5dbe64cd49096538536d.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         message.from_user.first_name
